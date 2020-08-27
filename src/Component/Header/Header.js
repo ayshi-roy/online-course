6de8b906fbbd089491a9ecb2a5a386c1ef9f57e8
ayshi-roy <ayshiroy22@gmail.com>
2020-08-27 21:36:49 +0600

    again checkup

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './Header.css'
-import Cart from '../Cart/Cart';
+
 
 const Header = () => {
     return (
